b90d570bdc681681c3cf10f846a3eac69270207e
ChristofferL <chrwadum@outlook.com>
2015-10-15 10:18:52 +0200

    Playercontroller ns

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,28 +1,30 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class PlayerController : MonoBehaviour
+namespace Assets.Scripts
 {
-
-    public float RotationMagnitude = 0.5f;
-    public float MovementMagnitude = 0.1f;
-
-    private Rigidbody rb;
-
-    private void Start()
+    public class PlayerController : MonoBehaviour
     {
-        rb = gameObject.GetComponent<Rigidbody>();
-    }
 
-    // Update is called once per frame
-	void Update () {
-        if (Input.GetKey("a"))
-            rb.AddTorque(new Vector3(0,1*RotationMagnitude,0));
-        if (Input.GetKey("d"))
-            rb.AddTorque(new Vector3(0, -1 * RotationMagnitude, 0));
-        if (Input.GetKey("w"))
-            rb.AddForce(transform.forward * MovementMagnitude);
-        if (Input.GetKey("s"))
-            rb.AddForce(transform.forward * -MovementMagnitude);
+        public float RotationMagnitude = 0.5f;
+        public float MovementMagnitude = 0.1f;
+
+        private Rigidbody _rb;
+
+        void Start()
+        {
+            _rb = gameObject.GetComponent<Rigidbody>();
+        }
+
+        // Update is called once per frame
+        void Update () {
+            if (Input.GetKey("a"))
+                _rb.AddTorque(new Vector3(0,1*RotationMagnitude,0));
+            if (Input.GetKey("d"))
+                _rb.AddTorque(new Vector3(0, -1 * RotationMagnitude, 0));
+            if (Input.GetKey("w"))
+                _rb.AddForce(transform.forward * MovementMagnitude);
+            if (Input.GetKey("s"))
+                _rb.AddForce(transform.forward * -MovementMagnitude);
+        }
     }
 }
