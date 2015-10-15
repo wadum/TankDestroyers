6904b4a7fd7343bbc6d9b43e35012e89e2b74ecd
Peter Troelsen <peter.j.troelsen@gmail.com>
2015-10-15 11:44:15 +0200

    Added a script that makes the camera follow the player around.

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,22 @@
+﻿using Assets.Scripts.Variables;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class CameraMovement : MonoBehaviour
+    {
+
+        private Transform _player;
+
+        // Use this for initialization
+        void Start ()
+        {
+            _player = GameObject.FindGameObjectWithTag(Constants.Tags.Player).transform;
+        }
+	
+        // Update is called once per frame
+        void Update () {
+            transform.position = new Vector3(_player.position.x, transform.position.y, _player.position.z);
+        }
+    }
+}
