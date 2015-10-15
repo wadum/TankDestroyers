a7ae3cdf49978caef36f9364ca344f685de43ecd
Peter Troelsen <peter.j.troelsen@gmail.com>
2015-10-15 10:21:00 +0200

    Added a script for enemy movement.  - Created a scene with nav mesh

---
+++ Assets/Scripts/Variables/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace Assets.Scripts.Variables
+{
+    public static class Constants {
+
+        public static class Tags
+        {
+            public static string Player = "Player";
+        }
+    }
+}
