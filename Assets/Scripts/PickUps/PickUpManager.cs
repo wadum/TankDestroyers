0d59f5c56951eba1323a51d0df9f707c232a9f78
ChristofferL <chrwadum@outlook.com>
2015-10-19 10:01:24 +0200

    Mine pick up - Needs sound

---
+++ Assets/Scripts/PickUps/PickUpManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.PickUps
+{
+    public class PickUpManager : MonoBehaviour
+    {
+
+        public GameObject PickUpModel;
+
+        void Start()
+        {
+            PickUpModel.transform.localPosition = Vector3.up*2;
+            PickUpModel.transform.localScale = Vector3.one * 4;
+            PickUpModel.transform.localRotation = Quaternion.Euler(0,0,45);
+            StartCoroutine(Rotate());
+        }
+
+        IEnumerator Rotate()
+        {
+            while (true)
+            {
+                PickUpModel.transform.Rotate(1,1,0);
+                yield return null;
+            }
+        }
+    }
+}
