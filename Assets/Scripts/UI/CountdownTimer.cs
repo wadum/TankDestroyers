cf8721dc56409f7f8a05d572c2af3571793bea71
Peter Troelsen <peter.j.troelsen@gmail.com>
2015-10-21 13:35:18 +0200

    Added playername to information in right corner of UI

--- Assets/Scripts/UI/CountdownTimer.cs
+++ Assets/Scripts/UI/CountdownTimer.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using Assets.Scripts.Variables;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
@@ -9,19 +11,38 @@ namespace Assets.Scripts
     {
         public long TimeLeft;
         private Text _text;
-        private RoundKeeper rc; 
+        private RoundKeeper rc;
+        private PlayerController pc;
 
         private void Start()
         {
             _text = GetComponent<Text>();
-            rc = GameObject.FindObjectOfType<RoundKeeper>();
+            rc = FindObjectOfType<RoundKeeper>();
+            FindPlayer();
+        }
+
+
+        private void FindPlayer()
+        {
+            var player = GameObject.FindGameObjectsWithTag(Constants.Tags.Player)
+                .FirstOrDefault(p => p.GetComponent<PlayerController>().isLocalPlayer);
+            if(player != null)
+                pc = player.GetComponent<PlayerController>();
         }
 
         // Update is called once per frame
-        void Update ()
+        private void Update()
         {
-            TimeLeft = rc.TimeLeft;
-            _text.text = String.Format("Time left: {0}:{1}", (TimeLeft / 60).ToString("00"), (TimeLeft % 60).ToString("00"));
+            if (pc == null)
+            {
+                FindPlayer();
+            }
+            else
+            {
+                TimeLeft = rc.TimeLeft;
+                _text.text = String.Format("{2} \nTime left: {0}:{1}", (TimeLeft/60).ToString("00"),
+                    (TimeLeft%60).ToString("00"), pc.PlayerName);
+            }
         }
     }
 }
